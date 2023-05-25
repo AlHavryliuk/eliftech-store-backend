92e460de91f4aaefc4dd8134ff97420dbb8ae685
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-05-25 18:54:23 +0300

    -storeId

--- models/orders.js
+++ models/orders.js
@@ -5,7 +5,6 @@ import { handleMongooseError } from "../helpers/handleMongooseError.js";
 const orderSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
-    storeId: { type: Schema.Types.ObjectId, ref: "stores", required: true },
     items: { type: Array, required: true },
     totalPrice: { type: Number },
   },
@@ -14,7 +13,6 @@ const orderSchema = new Schema(
 
 export const addOrderScheme = Joi.object({
   userId: Joi.string().hex().length(24).required(),
-  storeId: Joi.string().hex().length(24).required(),
   items: Joi.array().required(),
   totalPrice: Joi.number().required(),
 });
