1303ff1f5a4155c19bbb4cd296f37d45209c85ba
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-05-25 23:14:03 +0300

    +limite

--- controllers/stores.js
+++ controllers/stores.js
@@ -2,7 +2,7 @@ import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import { Stores } from "../models/stores.js";
 
 const getStores = async (req, res) => {
-  const { page = 1, limit = 6 } = req.query;
+  const { page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
   const totalStores = await Stores.countDocuments({});
   const stores = await Stores.find({}, "-createdAt -updatedAt", {
